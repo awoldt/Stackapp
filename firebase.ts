05f01c2db6ad04c2b6f8cb448b0c4c42581d31ed
awoldt <abreckwoldt23@gmail.com>
2023-08-03 19:02:11 -0700

    firebase credentials stored as env variables now

--- firebase.ts
+++ firebase.ts
@@ -1,10 +1,14 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app";
+import admin from "firebase-admin";
+import { getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import path from "path";
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert(path.join(__dirname, "..",  "..",  "..",  "firebase-admin-key.json")),
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
